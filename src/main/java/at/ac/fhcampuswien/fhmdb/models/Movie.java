ae301cbc216b92a69044494cb276ef2ea6d12cc1
Nour-hash <113339425+Nour-hash@users.noreply.github.com>
2023-03-07 23:16:47 +0100

    More Movies added

--- src/main/java/at/ac/fhcampuswien/fhmdb/models/Movie.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/models/Movie.java
@@ -48,9 +48,16 @@ public class Movie implements Comparable<Movie>{
     public static List<Movie> initializeMovies(){
         List<Movie> movies = new ArrayList<>();
         // add genres to the List
-        movies.add(new Movie("The Godfather", "A classic crime drama about the Corleone family.", Arrays.asList(Genre.DRAMA,Genre.ACTION,Genre.THRILLER)));
-        movies.add(new Movie("Star Wars: A New Hope", "A space opera about a group of rebels fighting against the evil Empire.", Arrays.asList(Genre.ACTION, Genre.ADVENTURE, Genre.FANTASY)));
-        movies.add(new Movie("Forrest Gump", "A comedy-drama about a man with a low IQ who witnesses and unwittingly influences several defining historical events in the 20th century USA.", Arrays.asList(Genre.COMEDY, Genre.DRAMA)));
+        movies.add(new Movie("The Godfather", "A classic crime drama about the Corleone family.", Arrays.asList(Movie.Genre.CRIME, Movie.Genre.DRAMA)));
+        movies.add(new Movie("Star Wars: A New Hope", "A space opera about a group of rebels fighting against the evil Empire.", Arrays.asList(Movie.Genre.ACTION, Movie.Genre.ADVENTURE, Movie.Genre.FANTASY)));
+        movies.add(new Movie("The Shawshank Redemption", "A drama about a man's journey in prison and his friendship with a fellow inmate.", Arrays.asList(Movie.Genre.DRAMA)));
+        movies.add(new Movie("The Lord of the Rings: The Fellowship of the Ring", "An epic fantasy adventure about a hobbit and his companions on a quest to destroy a powerful ring.", Arrays.asList(Movie.Genre.ADVENTURE, Movie.Genre.FANTASY)));
+        movies.add(new Movie("Forrest Gump", "A comedy-drama about a man with a low IQ who witnesses and unwittingly influences several defining historical events in the 20th century USA.", Arrays.asList(Movie.Genre.COMEDY, Movie.Genre.DRAMA)));
+        movies.add(new Movie("The Godfather: Part II", "A sequel to The Godfather, continuing the story of the Corleone family.", Arrays.asList(Movie.Genre.CRIME, Movie.Genre.DRAMA)));
+        movies.add(new Movie("The Dark Knight", "A superhero film featuring Batman as he battles the Joker and other villains in Gotham City.", Arrays.asList(Movie.Genre.ACTION, Movie.Genre.CRIME, Movie.Genre.DRAMA)));
+        movies.add(new Movie("The Matrix", "A science fiction action film about a computer hacker who discovers the truth about reality.", Arrays.asList(Movie.Genre.ACTION, Movie.Genre.SCIENCE_FICTION)));
+        movies.add(new Movie("Pulp Fiction", "A crime film featuring multiple storylines and characters in Los Angeles.", Arrays.asList(Movie.Genre.CRIME, Movie.Genre.DRAMA)));
+        movies.add(new Movie("Jurassic Park", "A science fiction adventure film about a theme park with genetically cloned dinosaurs.", Arrays.asList(Movie.Genre.ADVENTURE, Movie.Genre.SCIENCE_FICTION)));
         // return the list of movies
         return movies;
     }
